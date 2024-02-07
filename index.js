6ce294c8155baef6ddb1423aba9059c5a5d8a37a
veroniquex1 <vdrossouw@gmail.com>
2024-02-07 12:53:32 +0200

    server changes added

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+// Creating an HTTP Server
+// import http module
+const http = require("http");
 const {division} = require('./division.js')
 const {multiplication} = require('./multiplication.js')
-console.log(division(10, 5));
-console.log(multiplication(10, 10));
+// declare port number
+const portNo = +process.env.PORT || 3030;
+
+const server = http.createServer((req, res)=> {
+    // set the response header and content type that will be returned
+    res.writeHead(200, { 'Content-Type': 'text/plain' }); 
+
+    // set the response here
+    res.end(`Hello guys.\nFYI: This was made possible using the http core module\nYou are SO welcome!\nDivision: ${division(10, 5)}\nMultiplication: ${multiplication(10, 10)}`);
+});
+
+//  Listen on a specific port (in this case it is port 3030)
+server.listen(portNo, ()=> {
+    console.log(`The server is running at http://localhost:${portNo}`)
+
+});  
+
