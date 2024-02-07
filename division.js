d49aeb88f90ee6cafbd87ef2d34cb6b2d56fb4c7
thimnaalam <alamthimna@gmail.com>
2024-02-07 12:24:29 +0200

    modules

---
+++ division.js
@@ -0,0 +1,10 @@
+exports.division = (g, h)=>{
+     if (h <= 0){
+        console.log("Try a bigger number");
+    
+
+    }else {
+       return  g / h
+    }
+   
+}
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('API Presentation');
+const {division} = require('./division.js')
+const {multiplication} = require('./multiplication.js')
+console.log(division(10, 5));
+console.log(multiplication(10, 10));
